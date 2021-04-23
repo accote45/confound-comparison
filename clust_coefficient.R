8abc654d4cfbbba8a5a987aff4f501b3cce1f9e5
Alanna Cote <43280806+accote45@users.noreply.github.com>
2021-04-23 11:58:15 -0400

    Create clust_coefficient.R

---
+++ clust_coefficient.R
@@ -0,0 +1,29 @@
+
+
+
+library(tidyverse)
+library(plyr)
+library(data.table)
+library(purrr)
+library(igraph)
+library(reshape)
+library(qgraph)
+
+args<-commandArgs(trailingOnly=TRUE)
+folder=args[1]
+file=args[2]
+
+dat <- fread(paste0(folder,"/",file)) %>% column_to_rownames("V1")
+
+graph.obj<- graph_from_adjacency_matrix(as.matrix(cor(dat, method="pearson")), mode="undirected", weighted=TRUE, diag=FALSE)
+graph.obj <- simplify(graph.obj, remove.multiple=TRUE, remove.loops=TRUE)
+
+# Remove edges below absolute Pearson correlation 0.5
+graph.obj <- delete_edges(graph.obj, E(graph.obj)[which(abs(E(graph.obj)$weight)<0.5)])
+# Remove any vertices remaining that have no edges
+graph.obj <- delete.vertices(graph.obj, degree(graph.obj)==0)
+
+# calculate clustering coefficient per gene
+clust <- clustZhang(graph.obj)
+
+write.table(clust, paste0(file,"_clust.coeff"), quote=FALSE)
